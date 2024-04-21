a6e73b1d2ce9dc0fb82eb16d01e0bd56c9941236
dimazvg <zvg.dmtr.it@yandex.ru>
2024-04-21 16:06:16 +0300

    part 3.3.1

---
+++ 3. Первые программы на С. Типы данных/3.3.1.Arhimed.c
@@ -0,0 +1,10 @@
+#include <stdio.h> 
+
+int main() {
+    printf("Архимед вычислил число Пи ");
+    printf("как %d/%d или %f\n", 22, 7, 3.142857);
+    //printf("Arhimed calculate Pi number ");
+    //printf("like %d/%d or %f\n", 22, 7, 3.142857);
+    
+    return 0;
+} 
