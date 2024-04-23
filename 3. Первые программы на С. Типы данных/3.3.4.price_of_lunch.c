fd8bc605167346d0f31494ec82ead2b83a8e3b60
dimazvg <zvg.dmtr.it@yandex.ru>
2024-04-23 05:39:53 +0300

    3.3_23.04.24_05.39

---
+++ 3. Первые программы на С. Типы данных/3.3.4.price_of_lunch.c
@@ -0,0 +1,18 @@
+/*
+Студент хочет купить на обед в столовой 1 порцию риса за k_rice рублей и 2 салата за k_veg рублей каждый. 
+Сколько стоит обед?
+Sample Input:
+15 30
+Sample Output:
+75 
+*/
+#include <stdio.h>
+
+int main() {
+    int k_rice = 0, k_veg = 0;  // стоимость порции риса и салата соответственно
+    scanf("%d%d", &k_rice, &k_veg);
+    int cost_lunch = k_rice + 2 * k_veg;
+    printf("%d", cost_lunch);
+    
+    return 0;
+}
