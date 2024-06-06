93356631810c7d27e79f85df2ff6993f87590f66
dimazvg <zvg.dmtr.it@yandex.ru>
2024-06-07 02:33:54 +0300

    Part 4.1 changes

--- 4.  Функции (и указатели)/4.1.1.foo_and_hi.c
+++ 4.  Функции (и указатели)/4.1.1.foo_and_hi.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 
 void hi();
+void hi2(int group);
 float foo(float a, float b, float c, float x);  // axx + bx + c
 
 
 int main() {
     hi();       // call hi
+    hi2(951);
+    hi2(954);
+    hi2(978);
+    hi2(916);
     float y1, y2, y3;
     y1 = foo(1, 2, 3, 0.5);             // call foo
     y2 = foo(-3.1, 5.16, -0.01, 10);    // call foo
@@ -19,9 +24,13 @@ void hi() {
     return ;  // можно не писать
 }
 
+void hi2(int group) {
+    printf("Hello, group %d!\n", group);
+}
+
 float foo(float a, float b, float c, float x) {
     float res;
     res = a * x * x + b * x + c;
 
-    return res;
+    return res;  // return a * x * x + b * x + c;
 }
