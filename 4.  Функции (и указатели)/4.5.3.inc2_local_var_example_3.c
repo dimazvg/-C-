deb315c49ab7d96ca97f81aef1cbed694e08f229
dimazvg <zvg.dmtr.it@yandex.ru>
2024-06-07 15:43:38 +0300

    Part 4.5 changes

--- 4.  Функции (и указатели)/4.5.3.inc2_local_var_example_3.c
+++ 4.  Функции (и указатели)/4.5.3.inc2_local_var_example_3.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
-int inc (int x) {
+int inc(int x) {
     return x + 1;
 }
+
 int main () {
     int a = 3;
-    a = inc (a);
-    a = inc (a);
+    a = inc(a);
+    a = inc(a);
     printf("%d\n", a); // 5
 }
