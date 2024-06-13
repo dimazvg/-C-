879ae0338353a52a69a8a28b9afb4c9c7c6ded05
dimazvg <zvg.dmtr.it@yandex.ru>
2024-06-13 17:54:26 +0300

    3.7.5.concat_string.c 2

--- 3. Первые программы на С. Типы данных/3.7.5.concat_string.c
+++ 3. Первые программы на С. Типы данных/3.7.5.concat_string.c
@@ -1,7 +1,11 @@
+/* пример на конкатенацию (склейку строк) */
 #include <stdio.h>
 
 int main() {
     printf("Hello" ", " "world!" "\n");
+
+    int a = 2, b = 3;
+    printf("%d" " + " "%d" " = " "%d" "\n", a, b, a + b);
 
     return 0;
 }
